a9810c570ac38a053b1ef6e7560774ffd56cb763
Ryan Breuer <breuerryan@gmail.com>
2014-11-30 23:20:12 -0600

    Fix tabs to spaces

--- js/app/inventory.js
+++ js/app/inventory.js
@@ -1,8 +1,8 @@
 
 
 $(function() {
-	String.prototype.startsWith = function(str) 
-	{return (this.match("^"+str)==str)}
+    String.prototype.startsWith = function(str) 
+    {return (this.match("^"+str)==str)}
 
     var $itemToolTip = $("#itemTooltip");
     var $itemToolTipInn = $itemToolTip.find(".inn");
@@ -32,45 +32,45 @@ $(function() {
     });
     
     $(".inventory .slot").droppable({
-    	tolerance: "pointer",
-    	hoverClass: 'dropHover',
-    	drop: function( event, ui ) {
-			var $this = $(this);
-			var $draggable = ui.draggable;
-			var img = $draggable.find("img").attr('src');
-			var $detail = $draggable.find(".detail").html();
-			
-			$this.find(".detail").html($detail);
-			$this.find("img").attr('src', img);
-		}
+        tolerance: "pointer",
+        hoverClass: 'dropHover',
+        drop: function( event, ui ) {
+            var $this = $(this);
+            var $draggable = ui.draggable;
+            var img = $draggable.find("img").attr('src');
+            var $detail = $draggable.find(".detail").html();
+            
+            $this.find(".detail").html($detail);
+            $this.find("img").attr('src', img);
+        }
     });
     
     
     $.getJSON("http://api.wurstmineberg.de/minecraft/items/all.json", function(items) {
-    	var searchHtml = "";
-    	// console.log("Items: " + items.count, items.minecraft);
-    	$.each(items.minecraft, function(item) {
-    		searchHtml += '<div class="item">';
-    		var image = this.image;
+        var searchHtml = "";
+        // console.log("Items: " + items.count, items.minecraft);
+        $.each(items.minecraft, function(item) {
+            searchHtml += '<div class="item">';
+            var image = this.image;
 
-    		if (image && !image.startsWith("http")) {
-    			image = "http://assets.wurstmineberg.de/img/grid/" + image;
-    		}
-    		searchHtml += '<img src="' + image + '" />';
-    		searchHtml += '<div class="detail">';
+            if (image && !image.startsWith("http")) {
+                image = "http://assets.wurstmineberg.de/img/grid/" + image;
+            }
+            searchHtml += '<img src="' + image + '" />';
+            searchHtml += '<div class="detail">';
             searchHtml += '<span class="line">' + this.name + '</span>';
             searchHtml += '</div></div>';
-	    });
-	    
-	    $(".search").html(searchHtml);
-	    
-	    $(".search .item").draggable({
-	    	helper: "clone",
-	    	appendTo: ".container",
-	    	zIndex: 100,
-	    	revert: true,
-	    	revertDuration: 0
-	    });
+        });
+        
+        $(".search").html(searchHtml);
+        
+        $(".search .item").draggable({
+            helper: "clone",
+            appendTo: ".container",
+            zIndex: 100,
+            revert: true,
+            revertDuration: 0
+        });
     });
     
     
