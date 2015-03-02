09b4269f36de707ad48e0ed64ec784edcc28f201
Ryan Breuer <breuerryan@gmail.com>
2015-03-01 19:59:08 -0600

    Working configuration file

---
+++ mods/sponge/src/haveric/recipeManager/Files.java
@@ -0,0 +1,7 @@
+package haveric.recipeManager;
+
+
+public class Files {
+    public static final String NL = System.getProperty("line.separator");
+
+}
--- mods/sponge/src/haveric/recipeManager/RecipeManager.java
+++ mods/sponge/src/haveric/recipeManager/RecipeManager.java
@@ -2,12 +2,18 @@ package haveric.recipeManager;
 
 import haveric.recipeManager.commands.Commands;
 
+import java.io.File;
+
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.event.state.ServerAboutToStartEvent;
 import org.spongepowered.api.event.state.ServerStartingEvent;
 import org.spongepowered.api.event.state.ServerStoppingEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.service.config.DefaultConfig;
 import org.spongepowered.api.util.event.Subscribe;
 
 import com.google.inject.Inject;
@@ -18,9 +24,20 @@ import com.google.inject.Inject;
 public class RecipeManager {
 
     @Inject
+    @DefaultConfig(sharedRoot = false)
+    private File defaultConfig;
+
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    private File defaultFolder;
+    @Inject
     private Logger log;
 
     private Game game;
+    private Settings settings;
+
     private Commands commands;
 
     @Subscribe
@@ -31,6 +48,8 @@ public class RecipeManager {
     @Subscribe
     public void onStartup(ServerStartingEvent event) {
         commands = new Commands(this);
+
+        settings = new Settings(this, defaultConfig, configManager);
     }
 
     @Subscribe
