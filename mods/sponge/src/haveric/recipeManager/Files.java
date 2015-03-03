f1b2e7e87a2aecaf1761d39b4793fd14536769cf
Ryan Breuer <breuerryan@gmail.com>
2015-03-03 01:55:04 -0600

    Messing with name index

--- mods/sponge/src/haveric/recipeManager/Files.java
+++ mods/sponge/src/haveric/recipeManager/Files.java
@@ -7,14 +7,20 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Field;
+
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 
 
 public class Files {
     public static final String NL = System.getProperty("line.separator");
 
     public static final String FILE_USED_VERSION = "used.version";
+    public static final String FILE_INFO_NAMES = "name index.html";
 
     private static String DIR_PLUGIN;
+    private static final String SPONGE_DOCS = "";
 
     private RecipeManager plugin;
 
@@ -29,6 +35,8 @@ public class Files {
         createDirectories();
 
         boolean overwrite = isNewVersion();
+
+        createNameIndex(overwrite);
     }
 
     private void createDirectories() {
@@ -65,6 +73,75 @@ public class Files {
             Tools.saveTextToFile(currentVersion, file);
         }
 
-        return true;
+        return newVersion;
+    }
+
+    private boolean fileExists(String file, boolean overwrite) {
+        if (overwrite) {
+            return false;
+        }
+
+        return new File(DIR_PLUGIN + file).exists();
+    }
+
+    private void createNameIndex(boolean overwrite) {
+        if (fileExists(FILE_INFO_NAMES, overwrite)) {
+            return;
+        }
+
+        StringBuilder s = new StringBuilder(24000);
+
+        s.append("<title>Name index</title><pre style='font-family:Lucida Console;font-size:16px;width:100%;'>");
+        s.append(NL).append("<a href='basic recipes.html'>Basic Recipes</a> | <a href='advanced recipes.html'>Advanced Recipes</a> | <a href='recipe flags.html'>Recipe Flags</a> | <a href='recipe books.html'>Recipe Books</a> | <b>Name Index</b> | <a href='commands & permissions.html'>Commands &amp; Permissions</a>");
+        s.append(NL).append("<h1>Name index</h1>");
+        s.append(NL).append("Data extracted from your server and it may contain names added by other plugins/mods!");
+        s.append(NL).append("If you want to update this file just delete it and use '<i>rmreload</i>' or start the server.");
+        s.append(NL);
+        s.append(NL).append("<hr>");
+        s.append(NL);
+        s.append(NL).append("<a name='contents'></a><h3>CONTENTS</h3>");
+        s.append(NL).append("- <a href='#itemtypes'><b>ITEM TYPES</b></a>");
+
+
+        s.append(NL);
+        s.append(NL);
+        s.append(NL).append("<hr>");
+        s.append(NL);
+        s.append(NL).append("<a name='itemtypes'></a><a href='#contents'>^ Contents</a><h3>ITEM TYPES</h3>");
+        //s.append("<a href='" + SPONGE_DOCS + "Material.html'>BukkitAPI / Material</a>");
+        s.append(NL).append("Data/damage/durability values are listed at <a href='http://www.minecraftwiki.net/wiki/Data_value#Data'>Minecraft Wiki / Data Value</a>");
+        s.append(NL);
+        s.append(NL).append(String.format(" %-34s %-24s %-24s %-5s %s", "Id", "Name", "Alias", "Stack", "Max durability"));
+
+        for (Field field : ItemTypes.class.getDeclaredFields()) {
+            if (field.getType().equals(ItemType.class)) {
+                try {
+                    ItemType item = (ItemType) field.get(field);
+
+                    String alias = null;//Settings.getInstance().getMaterialPrint(m);
+
+                    String aliasString;
+                    if (alias == null) {
+                        aliasString = "";
+                    } else {
+                        aliasString = alias;
+                    }
+
+                    String durabilityString = "";
+                    if (item.getMaxDamage() != 0) {
+                        durabilityString += item.getMaxDamage();
+                    }
+
+                    s.append(NL).append(String.format(" %-34s %-24s %-24s %-5d %s", item.getId(), field.getName(), aliasString, item.getMaxStackQuantity(), durabilityString));
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+        Tools.saveTextToFile(s.toString(), DIR_PLUGIN + FILE_INFO_NAMES);
     }
 }
