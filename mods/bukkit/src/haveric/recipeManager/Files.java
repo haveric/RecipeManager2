f7c6912a3b34a89fdc988114c21cdd0edd0add89
Ryan Breuer <breuerryan@gmail.com>
2015-07-02 22:21:02 -0500

    2.5 Release

--- mods/bukkit/src/haveric/recipeManager/Files.java
+++ mods/bukkit/src/haveric/recipeManager/Files.java
@@ -48,7 +48,7 @@ public class Files {
     private final CommandSender sender;
     private static final String DIR_PLUGIN = RecipeManager.getPlugin().getDataFolder() + File.separator;
 
-    public static final String LASTCHANGED_CONFIG = "2.4.1";
+    public static final String LASTCHANGED_CONFIG = "2.5";
     public static final String LASTCHANGED_MESSAGES = "2.4";
     public static final String LASTCHANGED_ITEM_ALIASES = "2.4";
     public static final String LASTCHANGED_ENCHANT_ALIASES = "2.3";
