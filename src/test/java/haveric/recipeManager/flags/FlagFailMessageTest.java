d609cf9f35d88f57bd39345fdac21d79becbe5b8
Ryan Breuer <breuerryan@gmail.com>
2016-09-27 23:01:47 -0500

    Add testing for FlagFailMessage

---
+++ src/test/java/haveric/recipeManager/flags/FlagFailMessageTest.java
@@ -0,0 +1,46 @@
+package haveric.recipeManager.flags;
+
+import haveric.recipeManager.RecipeProcessor;
+import haveric.recipeManager.recipes.BaseRecipe;
+import haveric.recipeManager.recipes.CraftRecipe;
+import haveric.recipeManager.recipes.ItemResult;
+import haveric.recipeManagerCommon.RMCChatColor;
+import haveric.recipeManagerCommon.recipes.RMCRecipeInfo;
+import org.bukkit.Material;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlagFailMessageTest extends FlagBaseTest {
+
+    @Test
+    public void onRecipeParse() {
+        File file = new File("src/test/resources/recipes/flagFailMessage/");
+        RecipeProcessor.reload(null, true, file.getPath(), workDir.getPath());
+
+        Map<BaseRecipe, RMCRecipeInfo> queued = RecipeProcessor.getRegistrator().getQueuedRecipes();
+
+        assertEquals(2, queued.size());
+
+        for (Map.Entry<BaseRecipe, RMCRecipeInfo> entry : queued.entrySet()) {
+            CraftRecipe recipe = (CraftRecipe) entry.getKey();
+
+            Args a = ArgBuilder.create().recipe(recipe).build();
+            a.setPlayerUUID(testUUID);
+
+            ItemResult result = recipe.getResult(a);
+
+            FlagFailMessage flag = (FlagFailMessage) recipe.getFlag(FlagType.FAIL_MESSAGE);
+
+            Material resultType = result.getType();
+            if (resultType == Material.DIRT) {
+                assertEquals(RMCChatColor.COLOR_CHAR + "cYOU FAILED, MWaHahahah!", flag.getMessage());
+            } else if (resultType == Material.STONE_SWORD) {
+                assertEquals("Test", flag.getMessage());
+            }
+        }
+    }
+}
