8572d1ca8a3ee31460eb7d07af162e2e8536d0c1
ProgrammerDan <programmerdan@gmail.com>
2017-06-14 21:28:05 -0400

    Conform to project formatting guidelines

--- src/main/java/haveric/recipeManager/tools/RecipeIteratorV1_12.java
+++ src/main/java/haveric/recipeManager/tools/RecipeIteratorV1_12.java
@@ -21,46 +21,46 @@ import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.craftbukkit.v1_12_R1.inventory.RecipeIterator;
 
 public class RecipeIteratorV1_12 implements Iterator<Recipe> {
-	private RecipeIterator backing = null;
-	private Iterator<IRecipe> recipes = null;
-	private List<MinecraftKey> recipeRemoves = new LinkedList<MinecraftKey>();
-	private Iterator<ItemStack> smeltingCustom = null;
-	private List<ItemStack> recipeSmeltingCustom = new LinkedList<ItemStack>();
-	private Iterator<ItemStack> smeltingVanilla = null;
-	private List<ItemStack> recipeSmeltingVanilla = new LinkedList<ItemStack>();
-	enum RemoveFrom {
-		RECIPES,
-		CUSTOM,
-		VANILLA
-	}
-	RemoveFrom removeFrom = null;
-	IRecipe removeRecipe = null;
-	ItemStack removeItem = null;
-	
-	public RecipeIteratorV1_12(Iterator<Recipe> backing) {
-		if (backing instanceof RecipeIterator) {
-			backing = (RecipeIterator) backing;
-			recipes = CraftingManager.recipes.iterator();
-			smeltingCustom = RecipesFurnace.getInstance().customRecipes.keySet().iterator();
-			smeltingVanilla = RecipesFurnace.getInstance().recipes.keySet().iterator();
-		} else {
-			throw new IllegalArgumentException("This version is not supported.");
-		}
-	}
-	
-	/**
-	 * If nothing more is accessible, finalize any removals before informing caller of nothing new.
-	 */
-	@Override
-	public boolean hasNext() {
-		boolean next = recipes.hasNext() || smeltingCustom.hasNext() || smeltingVanilla.hasNext();
-		if (!next) {
-			finish();
-		}
-		return next;
-	}
+    private RecipeIterator backing = null;
+    private Iterator<IRecipe> recipes = null;
+    private List<MinecraftKey> recipeRemoves = new LinkedList<MinecraftKey>();
+    private Iterator<ItemStack> smeltingCustom = null;
+    private List<ItemStack> recipeSmeltingCustom = new LinkedList<ItemStack>();
+    private Iterator<ItemStack> smeltingVanilla = null;
+    private List<ItemStack> recipeSmeltingVanilla = new LinkedList<ItemStack>();
 
-	@Override
+    enum RemoveFrom {
+        RECIPES, CUSTOM, VANILLA
+    }
+
+    RemoveFrom removeFrom = null;
+    IRecipe removeRecipe = null;
+    ItemStack removeItem = null;
+
+    public RecipeIteratorV1_12(Iterator<Recipe> backing) {
+        if (backing instanceof RecipeIterator) {
+            backing = (RecipeIterator) backing;
+            recipes = CraftingManager.recipes.iterator();
+            smeltingCustom = RecipesFurnace.getInstance().customRecipes.keySet().iterator();
+            smeltingVanilla = RecipesFurnace.getInstance().recipes.keySet().iterator();
+        } else {
+            throw new IllegalArgumentException("This version is not supported.");
+        }
+    }
+
+    /**
+     * If nothing more is accessible, finalize any removals before informing caller of nothing new.
+     */
+    @Override
+    public boolean hasNext() {
+        boolean next = recipes.hasNext() || smeltingCustom.hasNext() || smeltingVanilla.hasNext();
+        if (!next) {
+            finish();
+        }
+        return next;
+    }
+
+    @Override
     public Recipe next() {
         if (recipes.hasNext()) {
             removeFrom = RemoveFrom.RECIPES;
@@ -83,93 +83,90 @@ public class RecipeIteratorV1_12 implements Iterator<Recipe> {
         }
     }
 
-	/**
-	 * Backing list is now immutable in 1.12. 
-	 * 
-	 * Instead of removing directly, we accrue removals, and apply them when instructed to
-	 * or when we reach the end of the list.
-	 */
+    /**
+     * Backing list is now immutable in 1.12.
+     * 
+     * Instead of removing directly, we accrue removals, and apply them when instructed to or when we reach the end of the list.
+     */
     public void remove() {
         if (removeFrom == null) {
             throw new IllegalStateException();
         }
-        switch(removeFrom) {
+        switch (removeFrom) {
         case RECIPES:
-        	try {
-				Field keyF = removeRecipe.getClass().getField("key");
-				MinecraftKey key = (MinecraftKey) keyF.get(removeRecipe);
-				System.err.println("Registered to remove " + key.toString());
-				recipeRemoves.add(key);
-        	} catch (Exception e) {
-        		e.printStackTrace();
-        	}
+            try {
+                Field keyF = removeRecipe.getClass().getField("key");
+                MinecraftKey key = (MinecraftKey) keyF.get(removeRecipe);
+                System.err.println("Registered to remove " + key.toString());
+                recipeRemoves.add(key);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         case CUSTOM:
-        	recipeSmeltingCustom.add(removeItem);
+            recipeSmeltingCustom.add(removeItem);
         case VANILLA:
-        	recipeSmeltingVanilla.add(removeItem);
+            recipeSmeltingVanilla.add(removeItem);
         }
     }
 
     public void finish() {
-    	if (!recipeRemoves.isEmpty()) {
-    		RegistryMaterials<MinecraftKey, IRecipe> existing = CraftingManager.recipes;
-    		CraftingManager.recipes = new RegistryMaterials<MinecraftKey, IRecipe>();
-    		try {
-    			Field c = CraftingManager.class.getDeclaredField("c");
-    			c.setAccessible(true);
-    			c.set(null, (Integer) 0); // reset recipe ID count.
-    			
-    			existing.iterator().forEachRemaining(recipe -> {
-					try {
-						Field keyF = recipe.getClass().getField("key");
-						MinecraftKey key = (MinecraftKey) keyF.get(recipe);
-						if (!recipeRemoves.contains(key)) {
-							CraftingManager.a(key, recipe); // just don't add ones we want to remove.
-						}
-					} catch (NoSuchFieldException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (SecurityException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IllegalArgumentException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-    			});
-    			
-    		} catch (Exception e) {
-				throw new IllegalStateException("Unable to access craft manager!");
-			}
-    	}
-    	if (!recipeSmeltingCustom.isEmpty()) {
-    		RecipesFurnace furnaces = RecipesFurnace.getInstance();
-    		recipeSmeltingCustom.forEach(
-    				item -> {
-    					furnaces.customRecipes.remove(item);
-    					furnaces.customExperience.remove(item);
-    				});
-    	}
-    	if (!recipeSmeltingVanilla.isEmpty()) {
-    		RecipesFurnace furnaces = RecipesFurnace.getInstance();
-    		try {
-    			Field experienceF = RecipesFurnace.class.getDeclaredField("experience");
-    			experienceF.setAccessible(true);
-    			@SuppressWarnings("unchecked")
-				Map<ItemStack, Float> experience = (Map<ItemStack, Float>) experienceF.get(furnaces); 
-	    		recipeSmeltingVanilla.forEach(
-	    				item -> {
-	    					furnaces.recipes.remove(item);
-	    					experience.remove(item);
-	    				});
-    		}catch (Exception e) {
-				// TODO: handle exception
-    			e.printStackTrace();
-			}
-	    		
-    	}
+        if (!recipeRemoves.isEmpty()) {
+            RegistryMaterials<MinecraftKey, IRecipe> existing = CraftingManager.recipes;
+            CraftingManager.recipes = new RegistryMaterials<MinecraftKey, IRecipe>();
+            try {
+                Field c = CraftingManager.class.getDeclaredField("c");
+                c.setAccessible(true);
+                c.set(null, (Integer) 0); // reset recipe ID count.
+
+                existing.iterator().forEachRemaining(recipe -> {
+                    try {
+                        Field keyF = recipe.getClass().getField("key");
+                        MinecraftKey key = (MinecraftKey) keyF.get(recipe);
+                        if (!recipeRemoves.contains(key)) {
+                            CraftingManager.a(key, recipe); // just don't add ones we want to remove.
+                        }
+                    } catch (NoSuchFieldException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (SecurityException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IllegalArgumentException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                });
+
+            } catch (Exception e) {
+                throw new IllegalStateException("Unable to access craft manager!");
+            }
+        }
+        if (!recipeSmeltingCustom.isEmpty()) {
+            RecipesFurnace furnaces = RecipesFurnace.getInstance();
+            recipeSmeltingCustom.forEach(item -> {
+                furnaces.customRecipes.remove(item);
+                furnaces.customExperience.remove(item);
+            });
+        }
+        if (!recipeSmeltingVanilla.isEmpty()) {
+            RecipesFurnace furnaces = RecipesFurnace.getInstance();
+            try {
+                Field experienceF = RecipesFurnace.class.getDeclaredField("experience");
+                experienceF.setAccessible(true);
+                @SuppressWarnings("unchecked")
+                Map<ItemStack, Float> experience = (Map<ItemStack, Float>) experienceF.get(furnaces);
+                recipeSmeltingVanilla.forEach(item -> {
+                    furnaces.recipes.remove(item);
+                    experience.remove(item);
+                });
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+
+        }
     }
 }
--- src/main/java/haveric/recipeManager/tools/ToolsRecipeV1_12.java
+++ src/main/java/haveric/recipeManager/tools/ToolsRecipeV1_12.java
@@ -23,166 +23,175 @@ import net.minecraft.server.v1_12_R1.ShapedRecipes;
 import net.minecraft.server.v1_12_R1.ShapelessRecipes;
 
 public class ToolsRecipeV1_12 {
-	public static boolean matches(BaseRecipe baseRecipe, Recipe bukkitRecipe) {
-		
-		if (bukkitRecipe instanceof CraftFurnaceRecipe) {
-			CraftFurnaceRecipe furnaceRecipe = (CraftFurnaceRecipe) bukkitRecipe;
-			if (baseRecipe instanceof SmeltRecipe) {
-				SmeltRecipe furnaceBase = (SmeltRecipe) baseRecipe;
-				// recipes are indexed by input, not result. So, if you overlap on input we don't care about output in terms of 
-				// the server's understanding of recipes.
-				return 
-						furnaceBase.getIngredient().getType() == furnaceRecipe.getInput().getType() &&
-							(furnaceBase.getIngredient().getDurability() == Vanilla.DATA_WILDCARD ||
-							furnaceBase.getIngredient().getDurability() == furnaceRecipe.getInput().getDurability());
-			}
-		} else if (bukkitRecipe instanceof CraftShapedRecipe) {
-			CraftShapedRecipe shapedRecipe = (CraftShapedRecipe) bukkitRecipe;
-			if (baseRecipe instanceof CraftRecipe) {
-				CraftRecipe craftBase = (CraftRecipe) baseRecipe;
-				
-				// Shortcut; if recipes don't have same geometry, not the same.
-				if (craftBase.getWidth() != shapedRecipe.getShape()[0].length() ||
-						craftBase.getHeight() != shapedRecipe.getShape().length) {
-						return false;
-				}
-				
-				// No such luck, but we do know geometry is the same and that is useful.
-				// roughly, we want to test, for each unique item type in the recipe, test it
-				// against every variation (until first successful match) of item types
-				// hidden by bukkit within the recipe definition.
+    public static boolean matches(BaseRecipe baseRecipe, Recipe bukkitRecipe) {
 
-				try {
-					Field recipeF = CraftShapedRecipe.class.getDeclaredField("recipe"); // pointer to MC recipe.
-					recipeF.setAccessible(true);
-					ShapedRecipes recipe = (ShapedRecipes) recipeF.get(shapedRecipe);
-					
-					Field itemsF = ShapedRecipes.class.getDeclaredField("items"); // pointer to _full_ list of items, hidden by Bukkit.
-					itemsF.setAccessible(true);
-					NonNullList<RecipeItemStack> items = (NonNullList<RecipeItemStack>) itemsF.get(recipe);
-					
-					// from Bukkit / spigot...
-					StringBuffer compair = new StringBuffer("   ");
-			        char c = 'a';
-			        int i = 0;
-			        org.bukkit.inventory.ItemStack[] baseItems = craftBase.getIngredients();
-			        compair.append(baseItems.length).append("v").append(items.size()).append("\n   ");
-			        for (RecipeItemStack list : items) {
-			        	org.bukkit.inventory.ItemStack baseItem = baseItems[ (i / craftBase.getWidth()) * 3 + (i % craftBase.getWidth()) ];
-			        	if (baseItem == null) {
-			        		baseItem = new org.bukkit.inventory.ItemStack(Material.AIR);
-			        	}
-			        	compair.append(" ").append(c).append("[").append(baseItem.toString()).append("]v[");
-			            if (list != null && list.choices.length > 0) {
-			            	boolean match = false;
-			            	for (ItemStack stack : list.choices) {
-			            	    org.bukkit.inventory.ItemStack bukkitItem = new org.bukkit.inventory.ItemStack(org.bukkit.craftbukkit.v1_12_R1.util.CraftMagicNumbers.getMaterial(stack.getItem()), 1, (short) stack.getData());
-			            	    compair.append(bukkitItem.toString()).append(",");
-			            	    if (bukkitItem.getType() == baseItem.getType() &&
-			            	    		(baseItem.getDurability() == Vanilla.DATA_WILDCARD ||
-			            	    		 bukkitItem.getDurability() == Vanilla.DATA_WILDCARD ||
-			            	    		 baseItem.getDurability() == bukkitItem.getDurability())) {
-			            	    	match = true;
-			            	    	break; // we need find only one match from all items. Stop when we have.
-			            	    }
-			            	}
-			            	compair.setLength(compair.length() - 1);
-			            	if (!match) {
-			            		compair.append("**FAIL*");
-			            		try {
-			            			//RecipeManager.getPlugin().getLogger().info(compair.toString());
-			            		} catch(Exception e){}
-			            		return false; // fast fail.
-			            	}
-			            } else {
-			            	if (baseItem.getType() == Material.AIR) {
-			            		compair.append("AIR");
-			            	} else {
-			            		compair.append("**FAIL*");
-			            		try {
-			            			//RecipeManager.getPlugin().getLogger().info(compair.toString());
-			            		} catch(Exception e){}
-			            		return false; // fast fail.
-			            	}
-			            }
-		            	compair.append("]");
-			            c++;
-			            i++;
-			            if (i % craftBase.getWidth() == 0) {
-			            	compair.append("\n   ");
-			            }
-			        }
-			        compair.append("\n   **MATCH*");
-            		try {
-            			//RecipeManager.getPlugin().getLogger().info(compair.toString());
-            		} catch(Exception e){}
-			        // if we made it through the whole list, and never failed to match, we have a match.
-			        return true;
+        if (bukkitRecipe instanceof CraftFurnaceRecipe) {
+            CraftFurnaceRecipe furnaceRecipe = (CraftFurnaceRecipe) bukkitRecipe;
+            if (baseRecipe instanceof SmeltRecipe) {
+                SmeltRecipe furnaceBase = (SmeltRecipe) baseRecipe;
+                // recipes are indexed by input, not result. So, if you overlap on input we don't care about output in terms of
+                // the server's understanding of recipes.
+                return furnaceBase.getIngredient().getType() == furnaceRecipe.getInput().getType()
+                        && (furnaceBase.getIngredient().getDurability() == Vanilla.DATA_WILDCARD || furnaceBase
+                                .getIngredient().getDurability() == furnaceRecipe.getInput().getDurability());
+            }
+        } else if (bukkitRecipe instanceof CraftShapedRecipe) {
+            CraftShapedRecipe shapedRecipe = (CraftShapedRecipe) bukkitRecipe;
+            if (baseRecipe instanceof CraftRecipe) {
+                CraftRecipe craftBase = (CraftRecipe) baseRecipe;
 
-				} catch (Exception e) {
-					try {
-						RecipeManager.getPlugin().getLogger().log(Level.WARNING, "Failed during recipe lookup", e);
-					} catch(Exception f) {
-						
-					}
-				}
-			}
-		} else if (bukkitRecipe instanceof CraftShapelessRecipe) {
-			CraftShapelessRecipe shapelessRecipe = (CraftShapelessRecipe) bukkitRecipe;
-			if (baseRecipe instanceof CombineRecipe) {
-				CombineRecipe combineBase = (CombineRecipe) baseRecipe;
-				
-				// Shortcut; if recipes don't have same # of itemtypes, not the same.
-				if (combineBase.getIngredients().size() != shapelessRecipe.getIngredientList().size()) {
-						return false;
-				}
-				
-				// No such luck, but we do know item size is the same and that is useful.
-				// roughly, we want to test, for each unique item type in the recipe, test it
-				// against every variation (until first successful match) of item types
-				// hidden by bukkit within the recipe definition.
+                // Shortcut; if recipes don't have same geometry, not the same.
+                if (craftBase.getWidth() != shapedRecipe.getShape()[0].length()
+                        || craftBase.getHeight() != shapedRecipe.getShape().length) {
+                    return false;
+                }
 
-				try {
-					Field recipeF = CraftShapelessRecipe.class.getDeclaredField("recipe"); // pointer to MC recipe.
-					recipeF.setAccessible(true);
-					ShapelessRecipes recipe = (ShapelessRecipes) recipeF.get(shapelessRecipe);
-					
-					Field itemsF = ShapelessRecipes.class.getDeclaredField("ingredients"); // pointer to _full_ list of items, hidden by Bukkit.
-					itemsF.setAccessible(true);
-					NonNullList<RecipeItemStack> items = (NonNullList<RecipeItemStack>) itemsF.get(recipe);
-					
-					// from Bukkit / spigot...
-			        int i = 0;
-			        List<org.bukkit.inventory.ItemStack> baseItems = combineBase.getIngredients();
-			        for (RecipeItemStack list : items) {
-			        	org.bukkit.inventory.ItemStack baseItem = baseItems.get(i);
-			            if (list != null && list.choices.length > 0) {
-			            	boolean match = false;
-			            	for (ItemStack stack : list.choices) {
-			            	    org.bukkit.inventory.ItemStack bukkitItem = new org.bukkit.inventory.ItemStack(org.bukkit.craftbukkit.v1_12_R1.util.CraftMagicNumbers.getMaterial(stack.getItem()), 1, (short) stack.getData());
-			            	    if (bukkitItem.getType() == baseItem.getType() &&
-			            	    		(baseItem.getDurability() == Vanilla.DATA_WILDCARD ||
-			            	    		 bukkitItem.getDurability() == Vanilla.DATA_WILDCARD ||
-			            	    		 baseItem.getDurability() == bukkitItem.getDurability())) {
-			            	    	match = true;
-			            	    	break; // we need find only one match from all items. Stop when we have.
-			            	    }
-			            	}
-			            	if (!match) {
-			            		return false; // fast fail.
-			            	}
-			            }
-			            i++;
-			        }
-			        // if we made it through the whole list, and never failed to match, we have a match.
-			        return true;
+                // No such luck, but we do know geometry is the same and that is useful.
+                // roughly, we want to test, for each unique item type in the recipe, test it
+                // against every variation (until first successful match) of item types
+                // hidden by bukkit within the recipe definition.
 
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		return false;
-	}
+                try {
+                    Field recipeF = CraftShapedRecipe.class.getDeclaredField("recipe"); // pointer to MC recipe.
+                    recipeF.setAccessible(true);
+                    ShapedRecipes recipe = (ShapedRecipes) recipeF.get(shapedRecipe);
+
+                    Field itemsF = ShapedRecipes.class.getDeclaredField("items"); // pointer to _full_ list of items, hidden by Bukkit.
+                    itemsF.setAccessible(true);
+                    NonNullList<RecipeItemStack> items = (NonNullList<RecipeItemStack>) itemsF.get(recipe);
+
+                    // from Bukkit / spigot...
+                    StringBuffer compair = new StringBuffer("   ");
+                    char c = 'a';
+                    int i = 0;
+                    org.bukkit.inventory.ItemStack[] baseItems = craftBase.getIngredients();
+                    compair.append(baseItems.length).append("v").append(items.size()).append("\n   ");
+                    for (RecipeItemStack list : items) {
+                        org.bukkit.inventory.ItemStack baseItem = baseItems[(i / craftBase.getWidth()) * 3
+                                + (i % craftBase.getWidth())];
+                        if (baseItem == null) {
+                            baseItem = new org.bukkit.inventory.ItemStack(Material.AIR);
+                        }
+                        compair.append(" ").append(c).append("[").append(baseItem.toString()).append("]v[");
+                        if (list != null && list.choices.length > 0) {
+                            boolean match = false;
+                            for (ItemStack stack : list.choices) {
+                                org.bukkit.inventory.ItemStack bukkitItem = new org.bukkit.inventory.ItemStack(
+                                        org.bukkit.craftbukkit.v1_12_R1.util.CraftMagicNumbers
+                                                .getMaterial(stack.getItem()),
+                                        1, (short) stack.getData());
+                                compair.append(bukkitItem.toString()).append(",");
+                                if (bukkitItem.getType() == baseItem.getType()
+                                        && (baseItem.getDurability() == Vanilla.DATA_WILDCARD
+                                                || bukkitItem.getDurability() == Vanilla.DATA_WILDCARD
+                                                || baseItem.getDurability() == bukkitItem.getDurability())) {
+                                    match = true;
+                                    break; // we need find only one match from all items. Stop when we have.
+                                }
+                            }
+                            compair.setLength(compair.length() - 1);
+                            if (!match) {
+                                compair.append("**FAIL*");
+                                try {
+                                    // RecipeManager.getPlugin().getLogger().info(compair.toString());
+                                } catch (Exception e) {
+                                }
+                                return false; // fast fail.
+                            }
+                        } else {
+                            if (baseItem.getType() == Material.AIR) {
+                                compair.append("AIR");
+                            } else {
+                                compair.append("**FAIL*");
+                                try {
+                                    // RecipeManager.getPlugin().getLogger().info(compair.toString());
+                                } catch (Exception e) {
+                                }
+                                return false; // fast fail.
+                            }
+                        }
+                        compair.append("]");
+                        c++;
+                        i++;
+                        if (i % craftBase.getWidth() == 0) {
+                            compair.append("\n   ");
+                        }
+                    }
+                    compair.append("\n   **MATCH*");
+                    try {
+                        // RecipeManager.getPlugin().getLogger().info(compair.toString());
+                    } catch (Exception e) {
+                    }
+                    // if we made it through the whole list, and never failed to match, we have a match.
+                    return true;
+
+                } catch (Exception e) {
+                    try {
+                        RecipeManager.getPlugin().getLogger().log(Level.WARNING, "Failed during recipe lookup", e);
+                    } catch (Exception f) {
+
+                    }
+                }
+            }
+        } else if (bukkitRecipe instanceof CraftShapelessRecipe) {
+            CraftShapelessRecipe shapelessRecipe = (CraftShapelessRecipe) bukkitRecipe;
+            if (baseRecipe instanceof CombineRecipe) {
+                CombineRecipe combineBase = (CombineRecipe) baseRecipe;
+
+                // Shortcut; if recipes don't have same # of itemtypes, not the same.
+                if (combineBase.getIngredients().size() != shapelessRecipe.getIngredientList().size()) {
+                    return false;
+                }
+
+                // No such luck, but we do know item size is the same and that is useful.
+                // roughly, we want to test, for each unique item type in the recipe, test it
+                // against every variation (until first successful match) of item types
+                // hidden by bukkit within the recipe definition.
+
+                try {
+                    Field recipeF = CraftShapelessRecipe.class.getDeclaredField("recipe"); // pointer to MC recipe.
+                    recipeF.setAccessible(true);
+                    ShapelessRecipes recipe = (ShapelessRecipes) recipeF.get(shapelessRecipe);
+
+                    Field itemsF = ShapelessRecipes.class.getDeclaredField("ingredients"); // pointer to _full_ list of items, hidden by Bukkit.
+                    itemsF.setAccessible(true);
+                    NonNullList<RecipeItemStack> items = (NonNullList<RecipeItemStack>) itemsF.get(recipe);
+
+                    // from Bukkit / spigot...
+                    int i = 0;
+                    List<org.bukkit.inventory.ItemStack> baseItems = combineBase.getIngredients();
+                    for (RecipeItemStack list : items) {
+                        org.bukkit.inventory.ItemStack baseItem = baseItems.get(i);
+                        if (list != null && list.choices.length > 0) {
+                            boolean match = false;
+                            for (ItemStack stack : list.choices) {
+                                org.bukkit.inventory.ItemStack bukkitItem = new org.bukkit.inventory.ItemStack(
+                                        org.bukkit.craftbukkit.v1_12_R1.util.CraftMagicNumbers
+                                                .getMaterial(stack.getItem()),
+                                        1, (short) stack.getData());
+                                if (bukkitItem.getType() == baseItem.getType()
+                                        && (baseItem.getDurability() == Vanilla.DATA_WILDCARD
+                                                || bukkitItem.getDurability() == Vanilla.DATA_WILDCARD
+                                                || baseItem.getDurability() == bukkitItem.getDurability())) {
+                                    match = true;
+                                    break; // we need find only one match from all items. Stop when we have.
+                                }
+                            }
+                            if (!match) {
+                                return false; // fast fail.
+                            }
+                        }
+                        i++;
+                    }
+                    // if we made it through the whole list, and never failed to match, we have a match.
+                    return true;
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return false;
+    }
 }
