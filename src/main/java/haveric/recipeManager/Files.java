f334e17d3bde12b8c4be907272c74644a3eee7ce
Ryan Breuer <breuerryan@gmail.com>
2018-12-09 23:16:39 -0600

    Update item aliases to handle old ids and remove most datas

--- src/main/java/haveric/recipeManager/Files.java
+++ src/main/java/haveric/recipeManager/Files.java
@@ -34,7 +34,7 @@ public class Files {
     public static final String LASTCHANGED_CONFIG = "2.12.0";
     public static final String LASTCHANGED_MESSAGES = "2.14.0";
     public static final String LASTCHANGED_ITEM_DATAS = "2.7";
-    public static final String LASTCHANGED_ITEM_ALIASES = "2.9";
+    public static final String LASTCHANGED_ITEM_ALIASES = "2.15.0";
     public static final String LASTCHANGED_ENCHANT_ALIASES = "2.11.0";
 
     public static final String FILE_CONFIG = "config.yml";
