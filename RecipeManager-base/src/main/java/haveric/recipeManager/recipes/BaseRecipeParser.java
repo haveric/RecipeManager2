286c74d6da999864f4ae555c8e7b0f1d511cba20
Ryan Breuer <breuerryan@gmail.com>
2019-09-03 18:50:37 -0500

    Bring fix over from branch

--- RecipeManager-base/src/main/java/haveric/recipeManager/recipes/BaseRecipeParser.java
+++ RecipeManager-base/src/main/java/haveric/recipeManager/recipes/BaseRecipeParser.java
@@ -93,7 +93,6 @@ public abstract class BaseRecipeParser {
 
                 for (ItemResult r : results) {
                     if (r.getChance() < 0) {
-                        totalPercentage -= r.getChance();
                         r.setChance(chance);
                         totalPercentage += chance;
                     }
