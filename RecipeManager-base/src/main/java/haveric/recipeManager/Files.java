e7e4554197d1b172611f3ef52e161f84b0fdd829
Ryan Breuer <breuerryan@gmail.com>
2019-09-10 17:47:00 -0500

    2.18.0 Release

--- RecipeManager-base/src/main/java/haveric/recipeManager/Files.java
+++ RecipeManager-base/src/main/java/haveric/recipeManager/Files.java
@@ -34,8 +34,8 @@ public class Files {
 
     private final CommandSender sender;
 
-    public static final String LASTCHANGED_CONFIG = "2.17.0";
-    public static final String LASTCHANGED_MESSAGES = "2.17.1";
+    public static final String LASTCHANGED_CONFIG = "2.18.0";
+    public static final String LASTCHANGED_MESSAGES = "2.18.0";
     public static final String LASTCHANGED_CHOICE_ALIASES = "2.17.0";
     public static final String LASTCHANGED_ITEM_DATAS = "2.7";
     public static final String LASTCHANGED_ITEM_ALIASES = "2.16.1";
