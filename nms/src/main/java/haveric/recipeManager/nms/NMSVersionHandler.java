fc13374e38ba0901b9f4990fab9515c059dfe7d6
Ryan Breuer <breuerryan@gmail.com>
2020-02-07 11:45:16 -0600

    Future proof the tools versioning

--- nms/src/main/java/haveric/recipeManager/nms/NMSVersionHandler.java
+++ nms/src/main/java/haveric/recipeManager/nms/NMSVersionHandler.java
@@ -35,18 +35,19 @@ public class NMSVersionHandler {
         if (toolsRecipe == null) {
             String serverVersion = getServerVersion();
 
-            switch (serverVersion) {
-                case "v1_15_R1":
-                case "v1_14_R1":
-                    toolsRecipe = new ToolsRecipeV1_14_R1();
-                    break;
-                case "v1_13_R2":
-                    toolsRecipe = new ToolsRecipeV1_13_2();
-                    break;
-                case "v1_12_R1":
-                default:
-                    toolsRecipe = new ToolsRecipeOld();
-                    break;
+            int minorVersion = getMinorVersion(serverVersion);
+            if (minorVersion >= 14) {
+                toolsRecipe = new ToolsRecipeV1_14_R1();
+            } else {
+                switch (serverVersion) {
+                    case "v1_13_R2":
+                        toolsRecipe = new ToolsRecipeV1_13_2();
+                        break;
+                    case "v1_12_R1":
+                    default:
+                        toolsRecipe = new ToolsRecipeOld();
+                        break;
+                }
             }
         }
 
@@ -56,5 +57,18 @@ public class NMSVersionHandler {
 
     private static String getServerVersion() {
         return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    }
+
+    private static int getMinorVersion(String serverVersion) {
+        int minorVersion = 0;
+        String[] versionSplit = serverVersion.split("_");
+
+        if (versionSplit.length == 3) {
+            try {
+                minorVersion = Integer.parseInt(versionSplit[1]);
+            } catch (NumberFormatException ignored) {}
+        }
+
+        return minorVersion;
     }
 }
